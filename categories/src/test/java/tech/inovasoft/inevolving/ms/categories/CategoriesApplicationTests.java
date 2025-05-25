deb80515670c79114d5c4fc73c13b38d04aa12ff
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-25 16:43:05 -0300

    Iniciando Projeto com Spring Boot

---
+++ categories/src/test/java/tech/inovasoft/inevolving/ms/categories/CategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.inovasoft.inevolving.ms.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
