bf360b263a2d957bf8e7743fe8d646cc3bf5a664
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-24 11:12:21 -0300

    Desenvolvendo Observabilidade de Error via Email

--- categories/src/main/java/tech/inovasoft/inevolving/ms/categories/controller/RestExceptionHandler.java
+++ categories/src/main/java/tech/inovasoft/inevolving/ms/categories/controller/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package tech.inovasoft.inevolving.ms.categories.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +11,13 @@ import tech.inovasoft.inevolving.ms.categories.domain.exception.ErrorInExternalS
 import tech.inovasoft.inevolving.ms.categories.domain.exception.NotFoundCategoryInDatabaseException;
 import tech.inovasoft.inevolving.ms.categories.domain.exception.NotFoundObjectiveInDatabaseException;
 
+@Slf4j
 @ControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(DataBaseException.class)
-    public ResponseEntity handleDataBaseException(DataBaseException exception) {
+    public ResponseEntity<ExceptionResponse> handleDataBaseException(DataBaseException exception) {
+        log.error("ERROR: {} - {}", exception.getClass().getSimpleName(), exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ExceptionResponse(
@@ -24,7 +27,9 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(ErrorInExternalServiceException.class)
-    public ResponseEntity handleErrorInExternalServiceException(ErrorInExternalServiceException exception) {
+    public ResponseEntity<ExceptionResponse> handleErrorInExternalServiceException(ErrorInExternalServiceException exception) {
+        log.error("ERROR: {} - {}", exception.getClass().getSimpleName(), exception.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ExceptionResponse(
@@ -34,7 +39,9 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(NotFoundCategoryInDatabaseException.class)
-    public ResponseEntity handleNotFoundCategoryInDatabaseException(NotFoundCategoryInDatabaseException exception) {
+    public ResponseEntity<ExceptionResponse> handleNotFoundCategoryInDatabaseException(NotFoundCategoryInDatabaseException exception) {
+        log.error("ERROR: {} - {}", exception.getClass().getSimpleName(), exception.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ExceptionResponse(
@@ -44,7 +51,9 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(NotFoundObjectiveInDatabaseException.class)
-    public ResponseEntity handleNotFoundObjectiveInDatabaseException(NotFoundObjectiveInDatabaseException exception) {
+    public ResponseEntity<ExceptionResponse> handleNotFoundObjectiveInDatabaseException(NotFoundObjectiveInDatabaseException exception) {
+        log.error("ERROR: {} - {}", exception.getClass().getSimpleName(), exception.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ExceptionResponse(
---
+++ categories/src/main/java/tech/inovasoft/inevolving/ms/categories/service/client/email_service/dto/EmailRequest.java
@@ -0,0 +1,4 @@
+package tech.inovasoft.inevolving.ms.categories.service.client.email_service.dto;
+
+public record EmailRequest(String to, String subject, String body) {
+}
